0740ea3686fb41a7ef585c1ee76c8aa6aa07cd49
mad.zero <33372049+Madhupal841998@users.noreply.github.com>
2022-12-20 17:49:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin: *');
+    res.setHeader('Access-Control-Allow-Origin',*');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
