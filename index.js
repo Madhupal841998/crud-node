34a551d368f9ddd07dc441f8c8db0325f0254624
mad.zero <33372049+Madhupal841998@users.noreply.github.com>
2022-12-20 19:34:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ const dotenv= require('dotenv').config();
 
 var app = express();
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin','*');
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-});
-
-
 
 app.use(cors({ origin: process.env.ORIGIN }));
 const PORT = process.env.PORT || 3030;
